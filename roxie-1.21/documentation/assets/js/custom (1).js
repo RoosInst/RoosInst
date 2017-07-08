126b32ab47232d668b036fad75ee88518690e3ff
jtheberge <jtheberge@outlook.com>
2017-07-07 18:28:38 -0700

    Updated favicons, changed brands color on roos-roxie-dark.css to be same as roos-roxie, deleted all old bookmark links and replaced with new ones, overall slight changes in header, changed footer RI icon to new logo

---
+++ roxie-1.21/documentation/assets/js/custom (1).js
@@ -0,0 +1,45 @@
+/*------------------------------
+ * Copyright 2016 Pixelized
+ * http://www.pixelized.cz
+ *
+ * Documentation for theme Roxie theme v1.3
+ * Pixelized studio - Front-end developers from Czech Republic
+ * info@pixelized.cz
+ * We do not support theme customizations beyond it's original functionality & appearance.
+------------------------------*/
+
+$(document).ready(function() {
+	$('#menu-open').click(function(e) {
+        $(this).toggleClass('show2 hidden');
+		$('#menu-close').toggleClass('show2 hidden');
+		$(".page-menu").toggleClass('page-menu-open');
+		$(".page-content").toggleClass('menu-is-open');
+    });
+	
+	$('#menu-close').click(function(e) {
+        $(this).toggleClass('show2 hidden');
+		$('#menu-open').toggleClass('show2 hidden');
+		$(".page-menu").toggleClass('page-menu-open');
+		$(".page-content").toggleClass('menu-is-open');
+    });
+		
+	$('.dropdown').on('show.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+	});
+	
+	$('.dropdown').on('hide.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+	});
+	
+	$("a.scroll[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({ scrollTop: $(this.hash).offset().top - 70}, 700, function(){window.location.hash = hash;});
+	});
+	
+	//MAGNIFIC POPUP
+	$('.show-image').magnificPopup({type:'image'});
+});
+
+
+
