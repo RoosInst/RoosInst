f5986ddaa355ff083f3fc5eaf0d00a52dc31080e
Unknown <jtheberge@outlook.com>
2018-04-09 18:26:28 -0700

    Add gulp synchronous & deleting old dist files support. Update bottom.

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,22 @@
-/*
-    Before using make sure you have:
-    npm install --save-dev gulp gulp-minify-css gulp-concat gulp-uglify gulp-autoprefixer gulp-sass gulp-htmlmin
- */
-var gulp = require('gulp'),
+/* Before using make sure you have:
+   npm install --save-dev gulp gulp-minify-css gulp-concat gulp-uglify gulp-autoprefixer gulp-sass gulp-htmlmin del
+*/
+
+const gulp = require('gulp'),
     minifyCSS = require('gulp-minify-css'),
     concat = require('gulp-concat')
     uglify = require('gulp-uglify')
     prefix = require('gulp-autoprefixer')
     htmlmin = require('gulp-htmlmin')
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    del = require('del');
 
-// Minifies JS
+// Minify & concat JS
 gulp.task('js', function(){
     return gulp.src('src/js/*.js')
     .pipe(uglify())
     .pipe(concat('roos.min.js'))
     .pipe(gulp.dest('dist/js'))
-});
-
-/*==========  Minify and concat different styles files  ==========*/
-
-// SASS Version
-// gulp.task('styles', function(){
-//     return gulp.src('src/sass/**/*.sass')
-//     .pipe(sass())
-//     .pipe(prefix('last 2 versions'))
-//     .pipe(concat('main.css'))
-//     .pipe(minifyCSS())
-//     .pipe(gulp.dest('public/css'))
-// });
-
-// SCSS Version
-//gulp.task('styles', function(){
-    //return gulp.src('src/scss/**/*.scss')
-    //.pipe(sass())
-    //.pipe(prefix('last 2 versions'))
-    //.pipe(concat('main.css'))
-    //.pipe(minifyCSS())
-    //.pipe(gulp.dest('public/css'))
-//});
-
-
-// CSS Version
-gulp.task('css', function(){
-    return gulp.src([
-      './src/css/!(roos)*.css', // all files that end in .js EXCEPT foobar*.js
-      './src/css/roos.css',
-    ])
-    .pipe(concat('roos.min.css'))
-    .pipe(minifyCSS())
-    .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest('dist/css'))
 });
 
 // Compiles .scss to .css
@@ -60,7 +26,19 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('src/css'))
 });
 
-// Gulp task to minify HTML files
+//Minify & concat CSS
+gulp.task('css', ['sass'], function() { //list sass as dependency, want to compile to scss to css first
+    return gulp.src([
+      './src/css/!(roos)*.css', // all .css files except roos.css (want roos css concat last, most important & CSS order matters)
+      './src/css/roos.css',
+    ])
+    .pipe(concat('roos.min.css'))
+    .pipe(minifyCSS())
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('dist/css'))
+});
+
+//Minify HTML
 gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(htmlmin({
@@ -74,9 +52,13 @@ gulp.task('html', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', function() {
-    gulp.run('sass') //sass before css, want to compile to scss to css first
-    gulp.run('css')
-    gulp.run('js')
-    gulp.run('html')
+//Remove old dist files
+gulp.task('clean', function() {
+     return del('dist/**', {force:true});
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('css');
+    gulp.start('js');
+    gulp.start('html');
 });
