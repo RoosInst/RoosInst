a59b76e1e7bc3fa0f1acc380c15a63d364cc8a92
Ryan Benech <rbenech@roos.com>
2021-02-03 18:53:45 -0500

    Files from home1 (github repo was old?)

--- src/js/roos.js
+++ src/js/roos.js
@@ -258,8 +258,6 @@ $(document).ready(function () {
 	replaceTitleAttributes();
 	//Removes default attributes of abbr tag, allows custom css
 
-
-	//TODO Remove to use GoogleSearchAPI in search5
 	//Google search
 	(function () {
 		var cx = '016663888408278794732:a1ud06__nsq';
@@ -271,8 +269,6 @@ $(document).ready(function () {
 		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(gcse, s);
 	})();
-
-
 	//replace sign in with username when signed in, requires js.cookies.js
 	if (Cookies.get("username")) {
 		$("#username").html("<i class='fa fa-user'></i>" + Cookies.get("username"));
@@ -453,20 +449,18 @@ function goSearch4() {
 // }
 
 function hasCookie() {
-	str = document.cookie.split('; ');
-	var result = {};
-	for (var i = 0; i < str.length; i++) {
-		var cur = str[i].split('=');
-		result[cur[0]] = cur[1];
-	}
-	if (result.username != null) {
-		return true;
-	}
-	return false;
+    str = document.cookie.split('; ');
+    var result = {};
+    for (var i = 0; i < str.length; i++) {
+        var cur = str[i].split('=');
+        result[cur[0]] = cur[1];
+    }
+    if (result.username != null){
+        return true;
+    }
+    return false;
 }
-
-// Integrated Web & Doc Search
-function doSearch5() {
+function doSearch5(){
 	var docSearch5 = window.document.docsSearch5;
 	var searchPhrase5 = docSearch5.query5.value.toString();
 	if (searchPhrase5 == '') {
@@ -476,103 +470,146 @@ function doSearch5() {
 		return true;
 	} else {
 		var searchString5 = escape(searchPhrase5).split("#")[0];
-		location.href = "/search.html?query=" + searchString5;
+		location.href = "/search.html?query=" + unescape(searchString5);
 		return;
 	}
 
 }
 
-function goSearch5(identifier = null, docs = null, query = null, target = null) {
+function searchModal(){
+	var searchString = escape(window.document.modalSearch.query.value);
+	switch($('#search_concept_modal').text()){
+		case 'All':
+			location.href = "/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=" + searchString;
+			break;
+		case 'Web':
+			location.href = "/search.html?query=" + searchString;
+			break;
+		case 'Documentation':
+			location.href = "/roos/documentation.nsf/webDocs?searchView&SearchFuzzy=TRUE&Query=" + searchString;
+			break;
+	}
+}
 
+function searchBar(){
+	var searchString = escape(window.document.search.query.value);
+	switch($('#search_concept').text()){
+		case 'All':
+			location.href = "/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=" + searchString;
+			break;
+		case 'Web':
+			location.href = "/search.html?query=" + searchString;
+			break;
+		case 'Documentation':
+			location.href = "/roos/documentation.nsf/webDocs?searchView&SearchFuzzy=TRUE&Query=" + searchString;
+			break;
+	}
+}
+
+function goSearch5(identifier = null, docs = null, query = null, target = null) {
+	
 	var docSearch5 = window.document.docsSearch5;
-	var searchPhrase5, start
-	if (identifier == null && query == null) {
-		searchPhrase5 = docSearch5.query5.value.toString();  //ensures numbers will be treated as strings
-		start = 1;
-	} else if (query != null) {
-		searchPhrase5 = query;  //ensures numbers will be treated as strings
-		start = 1;
-	} else {
-		searchPhrase5 = $(identifier).data('q');
-		start = $(identifier).data('s');
+	if (identifier == null && query == null){
+		var searchPhrase5 = docSearch5.query5.value.toString();  //ensures numbers will be treated as strings
+		var start = 1;	
+	}else if(query != null){
+		var searchPhrase5 = query;  //ensures numbers will be treated as strings
+		var start = 1;	
+		window.document.search.query.value = query;
+	}else{
+		var searchPhrase5 = $(identifier).data('q');
+		var start = $(identifier).data('s');
 	}
 
+	if(location.href.includes("search.html")){
+		$('#search_concept').text("Web");
+	}else if(location.href.includes("json")){
+		$('#search_concept').text("All");
+	}else{
+		$('#search_concept').text("Documentation");
+	}
+	
 	if (searchPhrase5 == '') {
 		$("#searchFail5").removeClass("hidden");
 		docSearch5.query5.value = "";
 		docSearch5.query5.focus();
 		return true;
 	} else {
-		var searchString5 = escape(searchPhrase5).split("#")[0];
-		if (hasCookie()) {
-			location.href = "/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=" + searchString5;
-			return;
-		}
+		var searchString5 = unescape(searchPhrase5).split("#")[0];
+		// if(hasCookie()){
+		// 	location.href = "/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=" + searchString5;
+		// 	return;
+		// }else{
+		// 	window.history.pushState("search", null, "?query="+searchString5);
+		// }
+
+		window.history.pushState("search", null, "?query="+searchString5);
 		var cx = '016663888408278794732:a1ud06__nsq';
-		$.get('https://www.googleapis.com/customsearch/v1/', { 'cx': cx, 'q': searchString5, 'start': start, 'key': 'AIzaSyAX2BF1-AFcEvBG6YPZs-6IS0fDFuSF4xo', 'num': 10 },
+		$.get('https://www.googleapis.com/customsearch/v1/', {'cx': cx, 'q' : searchString5, 'start': start, 'key' : 'AIzaSyAX2BF1-AFcEvBG6YPZs-6IS0fDFuSF4xo', 'num': 10},
 			function (data, textStatus, jqXHR) {  // success callback
-				currentPage = (data.queries.request[0].startIndex - 1) / 10 + 1
+				currentPage = (data.queries.request[0].startIndex - 1)/10 + 1
 				console.log(currentPage);
 				console.log(data);
-				if (!$("#searchFail5").hasClass("hidden")) {
+				if(!$("#searchFail5").hasClass("hidden")){
 					$("#searchFail5").addClass("hidden");
 				}
-				if (target == null) {
+				if(target == null){
 					target = '#search-results';
 				}
 				items = data.items;
-				if (docs != null) {
-					docsItems = docs.slice((currentPage - 1) * 10, currentPage * 10 - 1);
+				if(docs != null){
+					docsItems = docs.slice((currentPage - 1)* 10, currentPage * 10 - 1);
 					items.push(docsItems);
 				}
-				html = '<div class="alert alert-info" role="alert">Please <a href="/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=' + searchString5 + '" class="alert-link">sign in</a> to get documentation results</div>';
+				icon = '<svg style="vertical-align:middle; margin-right:6px;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M4 1h5v1H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V6h1v7a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2z"/><path d="M9 4.5V1l5 5h-3.5A1.5 1.5 0 0 1 9 4.5z"/><path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/></svg>';
+				html = '<div class="alert alert-info" role="alert">' + icon + 'Please <a href="/roos/documentation.nsf/json?searchView&SearchFuzzy=TRUE&Query=' + searchString5 + '" class="alert-link">sign in</a> to get documentation results</div>';
 				html += '<div>';
-				for (i = 0; i < items.length; ++i) {
+				icon = '<svg style="vertical-align:middle; margin-right:6px;" width="0.8em" height="0.8em" viewBox="0 0 16 16" class="bi bi-globe" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.018 7.5h2.49c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5zM2.255 4H4.09a9.266 9.266 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.024 7.024 0 0 0 2.255 4zM8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm-.5 1.077c-.67.204-1.335.82-1.887 1.855-.173.324-.33.682-.468 1.068H7.5V1.077zM7.5 5H4.847a12.5 12.5 0 0 0-.338 2.5H7.5V5zm1 2.5V5h2.653c.187.765.306 1.608.338 2.5H8.5zm-1 1H4.51a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm1 2.5V8.5h2.99a12.495 12.495 0 0 1-.337 2.5H8.5zm-1 1H5.145c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12zm-2.173 2.472a6.695 6.695 0 0 1-.597-.933A9.267 9.267 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM1.674 11H3.82a13.651 13.651 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm8.999 3.472A7.024 7.024 0 0 0 13.745 12h-1.834a9.278 9.278 0 0 1-.641 1.539 6.688 6.688 0 0 1-.597.933zM10.855 12H8.5v2.923c.67-.204 1.335-.82 1.887-1.855A7.98 7.98 0 0 0 10.855 12zm1.325-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm.312-3.5h2.49a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.91 4a9.277 9.277 0 0 0-.64-1.539 6.692 6.692 0 0 0-.597-.933A7.024 7.024 0 0 1 13.745 4h-1.834zm-1.055 0H8.5V1.077c.67.204 1.335.82 1.887 1.855.173.324.33.682.468 1.068z"/></svg>';				for (i = 0; i < items.length; ++i){
 
-					if (items[i].content != undefined) {
+					if (items[i].content != undefined){
 						content = items[i].content;
-					} else {
+					}else{
 						content = items[i].htmlSnippet;
 					}
-					if (content == undefined) {
+					if(content == undefined){
 						break;
 					}
-					content = content.replace(/(\r\n|\n|\r)/gm, "");
+					content = content.replace(/(\r\n|\n|\r)/gm,"");
 					background = ''
-					if (i % 2 == 1) {
+					if(i % 2 == 1){
 						background = 'background-color: aliceblue';
 					}
 
-					html += '<div class="result d-xl-flex" style="' + background + '"><div class="col d-flex"><a class="= d-xl-flex align-items-xl-start" href="' + items[i].formattedUrl + '" style="font-size: 20px; display:block;">' + items[i].htmlTitle + '</a><span class="d-xl-flex align-items-xl-end" style="display:block;">' + content + '</span></div></div>';
+					html += '<div class="result d-xl-flex" style="'+background+'"><div class="col d-flex"><a class="= d-xl-flex align-items-xl-start" href="'+ items[i].formattedUrl+'" style="font-size: 20px; display:block;">'+ icon + items[i].htmlTitle+'</a><span class="d-xl-flex align-items-xl-end" style="display:block;">'+content+'</span></div></div>';
 
 
 				}
 				html += '</div>'
 				html += '<div style="display: flex; justify-content:center; height: 48px; padding-top: 8px; align-items:center; position: relative;"><div style="display: flex; justify-content:center; height: 20px;width: 200px; position: absolute;">'
-				if (data.queries.previousPage != undefined) {
-					html += '<a class="d-xl-flex justify-content-xl-center" data-q="' + searchString5 + '" data-s="' + data.queries.previousPage[0].startIndex + '" onClick="goSearch5(this, target=target)" href="#" style="padding-right: 5px;padding-left: 5px;"><</a>'
+				if(data.queries.previousPage != undefined){
+					html += '<a class="d-xl-flex justify-content-xl-center" data-q="' +searchString5+'" data-s="'+data.queries.previousPage[0].startIndex + '" onClick="goSearch5(this, target=target)" href="#" style="padding-right: 5px;padding-left: 5px;"><</a>'
 				}
 
-				currentPage = (data.queries.request[0].startIndex - 1) / 10 + 1
+				currentPage = (data.queries.request[0].startIndex - 1)/10 + 1
+				
+				for (i = 1; i <= Math.ceil(data.searchInformation.totalResults/10) && i <= 10; ++i){
+					startIndex = (i - 1)*10 + 1;
+					if (i == currentPage){
+						html += '<a class="d-xl-flex justify-content-xl-center inactive" data-q="' +searchString5+'" data-s="'+startIndex+'" style="padding-right: 5px;padding-left: 5px;">'+i+'</a>'
 
-				for (i = 1; i <= Math.ceil(data.searchInformation.totalResults / 10) && i <= 10; ++i) {
-					startIndex = (i - 1) * 10 + 1;
-					if (i == currentPage) {
-						html += '<a class="d-xl-flex justify-content-xl-center inactive" data-q="' + searchString5 + '" data-s="' + startIndex + '" style="padding-right: 5px;padding-left: 5px;">' + i + '</a>'
-
-					} else {
-						html += '<a class="d-xl-flex justify-content-xl-center" data-q="' + searchString5 + '" data-s="' + startIndex + '" href="#" onClick="goSearch5(this, target=target)" style="padding-right: 5px;padding-left: 5px;">' + i + '</a>'
+					}else{
+						html += '<a class="d-xl-flex justify-content-xl-center" data-q="' +searchString5+'" data-s="'+startIndex+'" href="#" onClick="goSearch5(this, target=target)" style="padding-right: 5px;padding-left: 5px;">'+i+'</a>'
 					}
 				}
-				if (data.queries.nextPage != undefined && currentPage != 10) {
-					html += '<a class="d-xl-flex justify-content-xl-center" data-q="' + searchString5 + '" data-s="' + data.queries.nextPage[0].startIndex + '" onClick="goSearch5(this, target=target)" href="#" style="padding-right: 5px;padding-left: 5px;">></a>'
+				if(data.queries.nextPage != undefined && currentPage !=10){
+					html += '<a class="d-xl-flex justify-content-xl-center" data-q="' +searchString5+'" data-s="'+data.queries.nextPage[0].startIndex + '" onClick="goSearch5(this, target=target)" href="#" style="padding-right: 5px;padding-left: 5px;">></a>'
 				}
 				html += '</div>'
 
 				html += '</div>'
-				html += '<div class="spacer" style="clear: both;"></div>'
+				html+='<div class="spacer" style="clear: both;"></div>'
 				$(target).html(html);
-			}
+	  		}
 		)
 		return true;
 	}
